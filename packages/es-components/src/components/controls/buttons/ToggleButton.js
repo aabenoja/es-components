0a0096c8bddb9193559517521d72db6739c0af56
Alex Abenoja <abenojaalex@gmail.com>
2018-12-05 15:20:22 -0700

    Fix: closing the menu did not change ToggleButton state

--- packages/es-components/src/components/controls/buttons/ToggleButton.js
+++ packages/es-components/src/components/controls/buttons/ToggleButton.js
@@ -1,5 +1,5 @@
 /* eslint no-confusing-arrow: 0 */
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 
@@ -27,10 +27,16 @@ const StyledOutlineButton = styled(OutlineButton)`
 function ToggleButton(props) {
   const theme = useTheme();
   const [isPressed, setIsPressed] = useState(props.isPressed);
+  const prevPressed = useRef(props.isPressed);
 
   function toggleButton(event) {
     setIsPressed(!isPressed);
     props.onClick(event);
+  }
+
+  if (prevPressed.current !== props.isPressed) {
+    setIsPressed(props.isPressed);
+    prevPressed.current = props.isPressed;
   }
 
   const { styleType, size, block, isOutline, ...buttonProps } = props;
